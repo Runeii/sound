4584d17276456a72507d91e74c29e672da957939
fightwithcrayon <contact@andrewthomashill.co.uk>
2018-06-27 08:09:30 +0100

    Complete redevelopment

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,10 +4,22 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
+
+require('dotenv').config()
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
+
+firebase.initializeApp({
+  apiKey: process.env.FIREBASE_KEY,
+  authDomain: process.env.FIREBASE_DOMAIN,
+  databaseURL: process.env.FIREBASE_URL,
+  projectId: process.env.FIREBASE_ID,
+  storageBucket: process.env.FIREBASE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_SENDER
+})
 
 /* eslint-disable no-new */
 new Vue({
