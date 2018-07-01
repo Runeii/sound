6ed6d8c95541941ca07553256c80740b9dad1684
fightwithcrayon <contact@andrewthomashill.co.uk>
2018-07-01 21:20:19 +0100

    Begin building Player controller

--- src/renderer/store/modules/Player.js
+++ src/renderer/store/modules/Player.js
@@ -1,10 +1,38 @@
 const state = {
+  queue: [],
+  pointer: false
 }
 
 const getters = {
 }
 
 const mutations = {
+  // Queue
+  ADD_TO_QUEUE (state, uuid) {
+    Array.isArray(uuid) ? state.queue.push(...uuid) : state.queue.push(uuid)
+  },
+  ADD_TO_FRONT_OF_QUEUE (state, uuid) {
+    Array.isArray(uuid) ? state.queue.unshift(...uuid) : state.queue.unshift(uuid)
+  },
+  CLEAR_QUEUE (state) {
+    state.queue = []
+  },
+  REMOVE_FROM_QUEUE (state, index) {
+    state.queue = state.queue.splice(index, 0)
+  },
+  // Pointer
+  CLEAR_POINTER (state) {
+    state.pointer = false
+  },
+  POINTER_FORWARDS_ONE (state) {
+    state.pointer = state.pointer <= state.queue.length ? state.pointer + 1 : state.queue.length
+  },
+  POINTER_BACKWARDS_ONE (state) {
+    state.pointer = state.pointer > 0 ? state.pointer - 1 : 0
+  },
+  SET_POINTER (state, index) {
+    state.pointer = index
+  }
 }
 
 const actions = {
